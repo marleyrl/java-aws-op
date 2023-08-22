3cf7f328ba3c1c86fa89b2df5c169ea27fa0609f
Marley <marleyrlribeiro@gmail.com>
2023-08-21 22:56:09 -0300

    Implementação dos adicionarTarefa(String descricao): Adiciona uma nova tarefa à lista com a descrição fornecida. removerTarefa(String descricao): Remove uma tarefa da lista com base em sua descrição. obterNumeroTotalTarefas(): Retorna o número total de tarefas na lista. obterDescricoesTarefas(): Retorna uma lista contendo a descrição de todas as tarefas na lista.

---
+++ javaop/src/javaop/ListaTarefas.java
@@ -0,0 +1,55 @@
+package javaop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+//Atributos
+
+	private List<Tarefa> tarefaList;
+		
+	public ListaTarefas() {
+		this.tarefaList = new ArrayList<>();
+	
+	}
+	
+	public void adicionarTarefa(String descricao) {
+		tarefaList.add(new Tarefa(descricao));
+	}
+	
+	public void removerTarefa(String descricao) {
+		List<Tarefa> tarefaParaRemover = new ArrayList<>();
+		for(Tarefa t: tarefaList) {
+			if(t.getDescricao().equalsIgnoreCase(descricao)){
+				tarefaParaRemover.add(t);
+			}
+		}
+		tarefaList.removeAll(tarefaParaRemover);
+	}
+	
+	public int obterNumeroTotalTarefas() {
+		return tarefaList.size();
+	}
+	
+	public void obterDescricoesTarefas() {
+		System.out.println(tarefaList);
+	}
+	
+	public static void main (String [] args){
+		ListaTarefas listaTarefas = new ListaTarefas();
+		
+		listaTarefas.adicionarTarefa("Fazer Cafe");
+		listaTarefas.adicionarTarefa("Lavar Carro");
+		
+		
+		System.out.println(listaTarefas.obterNumeroTotalTarefas());
+		
+		listaTarefas.removerTarefa("Lavar Carro");
+		
+		System.out.println(listaTarefas.obterNumeroTotalTarefas());
+		
+		listaTarefas.obterDescricoesTarefas();
+		
+		System.out.println("Tarefa Acima");
+	}
+}	
